5d53624dc287e2a896ab30bc2cb60c4cb57cb31c
Joe Fearnley <joe.fearnley@gmail.com>
2024-09-02 22:21:20 -0400

    adding initial twitter data import

--- twitter/index.js
+++ twitter/index.js
@@ -1,9 +1,55 @@
 
 import { tweets } from './tweets.js';
+const https = require('https');
 
 tweets.forEach(tweet => {
-    console.log(`--------------------`);
-    console.log(tweet.tweet.full_text);
-    console.log(tweet.tweet.created_at);
-    console.log(`--------------------`);
+
+    const options = {
+        host: 'http://localhost:1337',
+        path: '/api/tweets',
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json; charset=UTF-8'
+        }
+    };
+
+
+
+    const body = {
+
+    }
+
+    // http://localhost:1337/api/tweets
+/*
+    retweeted
+source
+entities
+favorite_count
+in_reply_to_status_id_str
+id_str
+in_reply_to_user_id
+truncated
+retweet_count
+tweet_id
+in_reply_to_status_id
+tweet_created_at
+favorited
+full_text
+in_reply_to_screen_name
+in_reply_to_user_id_str
+*/
+
+fetch('http://localhost:1337/api/tweets', {
+    method: 'POST',
+    body: JSON.stringify({ title: 'foo', body: 'bar', userId: 1 }),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+})
+.then((response) => response.json())
+.then((json) => console.log(json))
+.catch(error => {
+    console.log(error)
+});
+
+    console.log(tweet.tweet.entities.media);
 });
