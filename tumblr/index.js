1116632f8afac64eff22f4585567c58a17e38832
Joe Fearnley <joe.fearnley@gmail.com>
2024-09-22 08:23:19 -0400

    Clneaning up tumblr import stuff

--- tumblr/index.js
+++ tumblr/index.js
@@ -1,62 +1,46 @@
 const fs = require('node:fs');
-const HTMLParser = require('node-html-parser');
-const minify = require('html-minifier').minify;
 const tumblr = require('tumblr.js');
 
-const client = tumblr.createClient({
-  consumer_key: process.env.TUMBLR_API_CONSUMER_KEY,
-  consumer_secret: process.env.TUMBLR_API_CONSUMER_SECRET,
-  token: process.env.TUMBLR_API_TOKEN,
-  token_secret: process.env.TUMBLR_API_TOKEN_SECRET
-});
+const getPostsFromApi = async () => {
+    const client = tumblr.createClient({
+        consumer_key: process.env.TUMBLR_API_CONSUMER_KEY,
+        consumer_secret: process.env.TUMBLR_API_CONSUMER_SECRET,
+        token: process.env.TUMBLR_API_TOKEN,
+        token_secret: process.env.TUMBLR_API_TOKEN_SECRET
+      });
+
+      let posts = [];
+      let response = null;
+  
+      for(let i = 1; i < 300; i = i + 50) {
+          response = await client.blogPosts('joefearnley', {
+              offset: i,
+              limit: 50,
+          });
+  
+          console.log(`adding ${response.posts.length} posts`);
+          console.log(``);
+  
+          posts.push(...response.posts);
+      }
+
+      return posts;
+};
+
 
 (async() => {
 
-    let posts = [];
-    let response = null;
-
-    for(let i = 1; i < 300; i = i + 50) {
-        response = await client.blogPosts('joefearnley', {
-            offset: i,
-            limit: 50,
-        });
-
-        console.log(`adding ${response.posts.length} posts`);
-        console.log(``);
-
-        posts.push(...response.posts);
-    }
+    const posts = getPostsFromApi();
 
     console.log(`total posts: ${posts.length}`);
-
+  
     fs.writeFileSync('blogposts.json', JSON.stringify(posts));
 
 })();
 
 
-const posts = JSON.parse(fs.readFileSync(`blogposts.json`, 'utf8'));
+const blogPosts = JSON.parse(fs.readFileSync(`blogposts.json`, 'utf8'));
 
-console.log(posts.length);
+console.log(blogPosts.length);
 
-console.log(response.posts.length);
-
-
-const readAHTMLFiles = () => {
-    const directory_name = './html';
-
-    const filenames = fs.readdirSync(directory_name); 
-
-    filenames.forEach((file) => {
-        const fileString = fs.readFileSync(`${directory_name}/${file}`, 'utf8');
-
-        root = HTMLParser.parse(fileString);
-
-        const bodyHTML = root.querySelector('body');
-
-        var result = minify(bodyHTML, {
-            removeAttributeQuotes: true
-        });
-
-        console.log(result);
-    });
-}
+// import posts into Strapi
