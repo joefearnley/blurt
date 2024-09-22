51b34f3ae951dcf8e8c52c1b376f6caa158a4e86
Joe Fearnley <joe.fearnley@gmail.com>
2024-09-22 08:17:01 -0400

    Correct the way api stuff is combined

--- tumblr/index.js
+++ tumblr/index.js
@@ -1,35 +1,8 @@
 const fs = require('node:fs');
-// const HTMLParser = require('node-html-parser');
-// const minify = require('html-minifier').minify;
-
-// const directory_name = './html';
-
-// const filenames = fs.readdirSync(directory_name); 
-
-// filenames.forEach((file) => {
-
-//     console.log(file);
-
-//     const fileString = fs.readFileSync(`${directory_name}/${file}`, 'utf8');
-
-//     root = HTMLParser.parse(fileString);
-
-//     console.log(file);
-//     console.log(fileString);
-//     console.log(root);
-
-//     const bodyHTML = root.querySelector('body');
-
-//     var result = minify(bodyHTML, {
-//         removeAttributeQuotes: true
-//       });
-
-//     console.log(result);
-
-// });
-
-
+const HTMLParser = require('node-html-parser');
+const minify = require('html-minifier').minify;
 const tumblr = require('tumblr.js');
+
 const client = tumblr.createClient({
   consumer_key: process.env.TUMBLR_API_CONSUMER_KEY,
   consumer_secret: process.env.TUMBLR_API_CONSUMER_SECRET,
@@ -38,14 +11,52 @@ const client = tumblr.createClient({
 });
 
 (async() => {
-    const response = await client.blogPosts('joefearnley');
 
-    console.log(JSON.stringify(response.posts.length));
+    let posts = [];
+    let response = null;
 
-    // fs.writeFileSync('response.txt', JSON.stringify(response));
+    for(let i = 1; i < 300; i = i + 50) {
+        response = await client.blogPosts('joefearnley', {
+            offset: i,
+            limit: 50,
+        });
+
+        console.log(`adding ${response.posts.length} posts`);
+        console.log(``);
+
+        posts.push(...response.posts);
+    }
+
+    console.log(`total posts: ${posts.length}`);
+
+    fs.writeFileSync('blogposts.json', JSON.stringify(posts));
+
 })();
 
 
-// const response = JSON.parse(fs.readFileSync(`response.json`, 'utf8'));
+const posts = JSON.parse(fs.readFileSync(`blogposts.json`, 'utf8'));
 
-// console.log(response.posts.length);
+console.log(posts.length);
+
+console.log(response.posts.length);
+
+
+const readAHTMLFiles = () => {
+    const directory_name = './html';
+
+    const filenames = fs.readdirSync(directory_name); 
+
+    filenames.forEach((file) => {
+        const fileString = fs.readFileSync(`${directory_name}/${file}`, 'utf8');
+
+        root = HTMLParser.parse(fileString);
+
+        const bodyHTML = root.querySelector('body');
+
+        var result = minify(bodyHTML, {
+            removeAttributeQuotes: true
+        });
+
+        console.log(result);
+    });
+}
