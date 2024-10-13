96e565c461548b3eb3f6f0ddafee976903aba471
Joe Fearnley <joe.fearnley@gmail.com>
2024-10-13 09:14:31 -0400

    Updating nav to have drop down menu

--- next-app/app/dashboard/layout.tsx
+++ next-app/app/dashboard/layout.tsx
@@ -4,8 +4,8 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
+    <section className="flex flex-col justify-center gap-4">
+      <div className="inline-block max-w-lg">
         {children}
       </div>
     </section>
--- next-app/components/navbar.tsx
+++ next-app/components/navbar.tsx
@@ -9,6 +9,14 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
+import { 
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem
+} from "@nextui-org/dropdown";
+import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
 import { usePathname } from 'next/navigation'
 import NextLink from "next/link";
@@ -31,30 +39,46 @@ export const Navbar = () => {
           </NextLink>
         </NavbarBrand>
         {siteConfig.navItems.map((item, i) => (
-          <NavbarItem 
-            key={i} 
-            isActive={pathname === item.href}
-            aria-current={pathname === item.href ? "page" : ""}
-            >
+          <NavbarItem key={i} isActive={pathname === item.href}>
             <Link href={item.href} color="foreground">
               {item.label}
             </Link>
           </NavbarItem>
         ))}
-      </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem>
-          <Link href={authenticated ? "/logout" : "/login"} color="foreground">
-            {authenticated ? "Log Out" : "Log In"}
-          </Link>
-        </NavbarItem>
         <NavbarItem className="hidden sm:flex gap-2">
           <ThemeSwitch />
         </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent
+          className="hidden sm:flex basis-1/5 sm:basis-full"
+          justify="end"
+        >
+        {authenticated ? (
+          <Dropdown>
+            <DropdownTrigger>
+              <Button variant="light">
+                @joefearnley
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                </svg>
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Static Actions">
+              {siteConfig.navMenuItems.map((item, i) => (
+                <DropdownItem key={`${i}`} variant="flat">
+                  <Link href={item.href} color="foreground">
+                    {item.label}
+                  </Link>
+                </DropdownItem>
+              ))}
+            </DropdownMenu>
+          </Dropdown>
+        ): (
+          <NavbarItem>
+            <Link href={"/login"} color="foreground">Log In</Link>
+          </NavbarItem>
+        )}
       </NavbarContent>
 
       {authenticated && (  
@@ -64,12 +88,8 @@ export const Navbar = () => {
           <NavbarMenu>
             <div className="mx-4 mt-2 flex flex-col gap-2">
               {siteConfig.navMenuItems.map((item, i) => (
-                <NavbarMenuItem key={`${item}-${i}`}>
-                  <Link
-                    color="foreground"
-                    href={item.href}
-                    size="lg"
-                  >
+                <NavbarMenuItem key={`${item.href}-${i}`}>
+                  <Link href={item.href} color="foreground" size="lg">
                     {item.label}
                   </Link>
                 </NavbarMenuItem>
