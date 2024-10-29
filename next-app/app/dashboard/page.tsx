e14b37110cce16fd85a45d9d3ae772950d927d1a
Joe Fearnley <joe.fearnley@gmail.com>
2024-10-28 22:11:30 -0400

    roughing in dashboard

--- next-app/app/dashboard/page.tsx
+++ next-app/app/dashboard/page.tsx
@@ -4,6 +4,10 @@ export default function DashboardPage() {
   return (
     <div>
       <h1 className={title()}>Dashboard</h1>
+
+      <div className="view">Views</div>
+      <div className="clicks">Clicks</div>
+      <div className="comments">Comments</div>
     </div>
   );
 }
