49c20f6da6e3737b44506f2a28a46fc1f14b42c2
Joe Fearnley <joe.fearnley@gmail.com>
2024-11-14 22:30:26 -0500

    Adding profile page

---
+++ next-app/app/profile/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { UserAccountSettingsForm } from "../../components/settings/UserAccountSettingsForm.jsx"
+import { PasswordResetForm } from "../../components/settings/PasswordResetForm.jsx"
+
+export default function ProfilePage() {
+  return (
+    <div className="flex flex-col md:flex-row gap-12">
+      <h2>Profile....</h2>
+    </div>
+  );
+}
