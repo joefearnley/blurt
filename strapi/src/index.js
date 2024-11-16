87575520b3b859b580d70f3ea6c1855868d505ef
Joe Fearnley <joe.fearnley@gmail.com>
2024-11-15 23:44:59 -0500

    Setting up initial socket io stuff

--- strapi/src/index.js
+++ strapi/src/index.js
@@ -16,5 +16,52 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap(/*{ strapi }*/) {
+
+      var io = require("socket.io")(strapi.server.httpServer, {
+        cors: {
+          origin: "http://localhost:3000",
+          methods: ["GET", "POST"],
+          allowedHeaders: ["my-custom-header"],
+          credentials: true,
+        },
+      });
+
+      io.on("connection", function (socket) {
+        socket.on("join", ({ username }) => {
+          console.log("user connected");
+          console.log("username is ", username);
+
+          if (username) {
+            socket.join("group");
+            socket.emit("welcome", {
+              user: "bot",
+              text: `${username}, Welcome to the group chat`,
+              userData: username,
+            });
+          } else {
+            console.log("An error occurred");
+          }
+        });
+
+        socket.on("sendMessage", async (data) => {
+          let strapiData = {
+            data: {
+              user: data.user,
+              message: data.message,
+            },
+          };
+          var axios = require("axios");
+          await axios
+            .post("http://localhost:1337/api/messages", strapiData)
+            .then((e) => {
+              socket.broadcast.to("group").emit("message", {
+                user: data.username,
+                text: data.message,
+              });
+            })
+            .catch((e) => console.log("error", e.message));
+        });
+      });
+  },
 };
